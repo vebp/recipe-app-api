5623d85e6da3b28e77786a85f8b5ba903d4dd2fc
vistoco <vebustos1986@gmail.com>
2025-04-17 22:36:51 -0400

    build: section 10 done

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,54 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# this line will automate the translation on django pages
+# (if changed the language)
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+# NOTE: we have to add the comma at the end because as
+# there is only one element, django knows this is a tuple
+# on line 49
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
